7f29bfd97d3c7aba64f4b33889b35c9027dc1109
ShogoOnoda <167851307+onosan13@users.noreply.github.com>
2025-05-25 17:00:30 +0900

    ファイル追加

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
+  <script src="jquery/jquery-3.7.1.min.js"></script>
   <title>牛乳キャップバトル</title>
 </head>
 <body>
