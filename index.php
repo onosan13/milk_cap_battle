2713a400cc8b037c2e8984129c82e8c3e6724850
ShogoOnoda <167851307+onosan13@users.noreply.github.com>
2025-05-25 17:18:08 +0900

    タイトルページ仮配置

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +11,12 @@
   <script src="jquery/jquery-3.7.1.min.js"></script>
   <title>牛乳キャップバトル</title>
 </head>
+
 <body>
-  
+  <div class="title_wrapper">
+    <div class="title_logo_container">タイトルロゴ場所</div>
+    <div class="btn_container">スタートボタン</div>
+  </div>
 </body>
+
 </html>
